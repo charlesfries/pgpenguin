488d39c8935d71f55d63c3bf6d25549403023cec
Charles Fries <charliefries@icloud.com>
2020-04-26 16:57:08 -0700

    Cleanup

--- app/components/key-dropdown.js
+++ app/components/key-dropdown.js
@@ -1,4 +1,31 @@
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class KeyDropdownComponent extends Component {
+
+	@tracked selectedKey;
+	@tracked publicOpen;
+	@tracked privateOpen;
+
+	@action showPublic(key) {
+		this.selectedKey = key;
+		this.publicOpen = true;
+	}
+
+	@action hidePublic() {
+		this.selectedKey = null;
+		this.publicOpen = false;
+	}
+
+	@action showPrivate(key) {
+		this.selectedKey = key;
+		this.privateOpen = true;
+	}
+
+	@action hidePrivate() {
+		this.selectedKey = null;
+		this.privateOpen = false;
+	}
+
 }
--- app/helpers/timestamp.js
+++ app/helpers/timestamp.js
@@ -1,5 +1,5 @@
 import { helper } from '@ember/component/helper';
 
 export default helper(function timestamp([date]) {
-  return date.toLocaleString();
+  return date ? date.toLocaleString() : null;
 });
