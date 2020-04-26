8f1802e9e061dcf11180d71be6e9665ef6b6f4c1
Charles Fries <charliefries@icloud.com>
2020-04-26 16:38:34 -0700

    Ch

--- app/models/key.js
+++ app/models/key.js
@@ -2,6 +2,17 @@ import Model, { attr } from '@ember-data/model';
 
 export default class KeyModel extends Model {
 	@attr('date', { defaultValue() { return new Date(); } }) createdAt;
+	@attr('string') nickname;
 	@attr('string') privateKey;
 	@attr('string') publicKey;
+
+	// async get name() {
+	// 	let thing = await openpgp.key.readArmored(this.publicKey);
+	// 	return thing;
+	// }
+
+	// get email() {
+	// 	let thing = await openpgp.key.readArmored(this.publicKey);
+	// 	return thing;
+	// }
 }
