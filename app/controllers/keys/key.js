0bbedc63099b863f2ffa886a0b00c3038d1b4087
Charles Fries <charliefries@icloud.com>
2020-04-27 16:35:09 -0700

    Footer changes

--- app/controllers/keys/key.js
+++ app/controllers/keys/key.js
@@ -1,10 +1,11 @@
 import Controller from '@ember/controller';
+import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class KeysKeyController extends Controller {
 
-	privateOpen;
-	publicOpen;
+	@tracked privateOpen;
+	@tracked publicOpen;
 
 	@action copyPrivate() {
 	}
--- app/routes/keys/key.js
+++ app/routes/keys/key.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default class KeysKeyRoute extends Route {
+
+	resetController(controller, isExiting) {
+		if (isExiting) {
+			controller.privateOpen = null;
+			controller.publicOpen = null;
+		}
+	}
+
 }
