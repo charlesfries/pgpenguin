16658e7424c2a954864fa284813d5b916438127b
Charles Fries <charliefries@icloud.com>
2020-04-27 13:39:24 -0700

    Changes

--- app/controllers/generate.js
+++ app/controllers/generate.js
@@ -4,15 +4,19 @@ import { action } from '@ember/object';
 
 export default class GenerateController extends Controller {
 
+	@tracked nickname;
 	@tracked name;
 	@tracked email;
 	@tracked key;
 
 	@action async submit() {
+		let { nickname } = this;
+		
 		let result = await openpgp.generateKey({ curve: 'brainpoolP512r1', userIds: [{ name: 'Test', email: 'test@test.com' }] });
 		try {
 
 			let key = this.store.createRecord('key', {
+				nickname,
 				privateKey: result.privateKeyArmored,
 				publicKey: result.publicKeyArmored
 			});
