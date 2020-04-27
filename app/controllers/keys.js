34e513e49e27c3e3fa70757805a4e7872c098e6a
Charles Fries <charliefries@icloud.com>
2020-04-27 16:46:54 -0700

    Cleanup

--- app/controllers/keys.js
+++ app/controllers/keys.js
@@ -1,9 +1,4 @@
 import Controller from '@ember/controller';
 
 export default class KeysController extends Controller {
-
-	get keys() {
-		return this.model.sortBy('id');
-	}
-
 }
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default class ApplicationRoute extends Route {
-	model() {
-		return this.store.findAll('key');
+
+	async model() {
+		let keys = await this.store.findAll('key');
+		return keys.sortBy('id');
 	}
+	
 }
